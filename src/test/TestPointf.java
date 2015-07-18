5527fb0404dc52f77e0745a56ff4bbe795ed105d
桑桑呜 <461945773@QQ.COM>
2015-07-19 00:54:27 +0800

    2015/07/19  0:54:27

--- src/test/TestPointf.java
+++ src/test/TestPointf.java
@@ -3,11 +3,11 @@ package test;
 import junit.framework.TestCase;
 import mid.Pointf;
 
-public class TestPointf extends TestCase{
-    public void testAll(){ 
-    	Pointf p = new Pointf(11.2f, 12.12f);
-        assertEquals("error P->x", p.getX(), 11.2f);
-        assertEquals("error P->y", p.getY(), 12.12f);
-        assertEquals("error P->distance", Pointf.distance(new Pointf(1, 1), new Pointf(4, 5)), 5f);
-    } 
+public class TestPointf extends TestCase {
+	public void testAll() {
+		Pointf p = new Pointf(11.2f, 12.12f);
+		assertEquals("error P->x", 11.2f, p.getX());
+		assertEquals("error P->y", 12.12f, p.getY());
+		assertEquals("error P->distance", 5f, Pointf.distance(new Pointf(1, 1), new Pointf(4, 5)));
+	}
 }
--- src/test/TestTriangle.java
+++ src/test/TestTriangle.java
@@ -2,10 +2,26 @@ package test;
 
 import junit.framework.TestCase;
 import mid.Pointf;
+import mid.Triangle;
 
-public class TestTriangle extends TestCase{
-    public void testAll(){ 
-    
-        assertEquals();
-    } 
+public class TestTriangle extends TestCase {
+	public void testR() {
+		for (int i = 0; i < 10; i++) {
+			float rand = (float) Math.random();
+			Triangle t = new Triangle(new Pointf(0 + rand, 3 + rand), new Pointf(0 + rand, -3 + rand),
+					new Pointf(3 + rand, 0 + rand));
+			assertEquals("error Triangle->getR", 3f, t.getR());
+		}
+	}
+
+	public void testMid(){
+    	for(int i = 0;i<10;i++){
+    		Triangle t = new Triangle(new Pointf(1,1),new Pointf(1, 4),new Pointf(5, 4));
+            assertEquals("error Triangle->getMid",3f,t.getMid().getX());
+            assertEquals("error Triangle->getMid",2.5f,t.getMid().getY());
+    	}
+    }
+	public void testDraw(){
+		System.out.println("Draw a circle");
+	}
 }
